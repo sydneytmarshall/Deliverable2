6abdb7cbdc1a806b4bb7305847e0a7ae5b3cb6e5
sydneytmarshall <122946501+sydneytmarshall@users.noreply.github.com>
2023-01-24 22:20:35 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿/* Author: Sydney Marshall
+ * Date: 01/22/2023
+ * Description: C# Console Application for calculating letter grades
+ */
+
+using System;
+
+namespace Deliverable_2_;
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+    
+
+        try {
+
+            Console.WriteLine("Enter a numeric grade");
+
+            int grade = int.Parse(Console.ReadLine());
+
+            string letter;
+
+            if (grade >= 90)
+        {
+            letter = "A";
+                Console.WriteLine("Your expected letter grade for ISM 4300 is:" + letter);
+
+            }
+
+        else if (grade >= 80 && grade < 90)
+        {
+            letter = "B";
+                Console.WriteLine("Your expected letter grade for ISM 4300 is:" + letter);
+            }
+        else if (grade >= 70 && grade < 80)
+        {
+            letter = "C";
+                Console.WriteLine("Your expected letter grade for ISM 4300 is:" + letter);
+            }
+        else if (grade >= 60 && grade < 70)
+        {
+            letter = "D";
+                Console.WriteLine("Your expected letter grade for ISM 4300 is:" + letter);
+            }
+        else if (grade <= 50)
+        {
+            letter = "F";
+                Console.WriteLine("Your expected letter grade for ISM 4300 is:" + letter);
+            }
+
+    }
+        catch
+        {
+            Console.WriteLine("Please enter a numeric value from 0 to 100");
+        
+    }
+    }
+}
+
